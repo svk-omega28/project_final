89b53c7b4f14107cc079a0cdaf944fa90a150664
Vladyslav <fallenangel281196@icloud.com>
2025-03-03 04:53:06 +0100

    Update CartCheckout.jsx

--- src/components/CartCheckout/CartCheckout.jsx
+++ src/components/CartCheckout/CartCheckout.jsx
@@ -1,1 +1,27 @@
+import { useContext } from 'react';
+import './CartCheckout.css';
+import { BsTrash } from 'react-icons/bs';
+import { HeartedContext } from '../../contexts/HeartedContext';
+import { Link } from 'react-router-dom';
+
+function CartCheckout({ productsAdded }) {
+  const { removeProduct } = useContext(HeartedContext);
+
+  return (
+    <div className='product-items'>
+      <Link to={`/details/${productsAdded?.id}`}>
+        <img src={productsAdded?.image} alt={productsAdded?.title} />
+      </Link>
+      <div className='title-wrap'>
+        <p>{productsAdded?.title}</p>
+      </div>
+      <p>${productsAdded?.price.toFixed(2)}</p>
+      {/* Отображаем quantity из productsAdded */}
+      <p>{productsAdded?.quantity || 1}</p>
+      <BsTrash onClick={() => removeProduct(productsAdded?.id)} className='trash-icon' />
+    </div>
+  );
+}
+
+export default CartCheckout;
 
